505b767a8c4558521053a5a37da7c04cc0d328f1
ThisCocaineMakesMeFeelLikeImOnThisRPG <51780329+Traitor1231@users.noreply.github.com>
2019-09-30 00:47:00 +0400

    Delete standart_int.js

--- src/js/standart_int.js
+++
@@ -1,6 +0,0 @@
-    var reload = localStorage.getItem("reloaded");
-      if (reload === null){
-        var target = document.getElementById('standart-int');
-        target.insertAdjacentHTML('beforeEnd', '<div class="intres">компьютеры</div><div class="intres">радио</div><div class="intres">музыка</div>');
-      }
-      localStorage.setItem("reloaded", true);
