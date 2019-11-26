b8e9421de5b921f0853320d14cb27e7ae611b818
Nikita Pereskokov <51780329+Traitor1231@users.noreply.github.com>
2019-11-26 19:10:07 +0400

    add module system

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import jQuery from "jquery";
+
+window.$ = window.jQuery = jQuery;
+
+$("input").attr("autocomplete", "off");
+
+require('./modules/prettify');
+
+require('./modules/jquery.scrollbar');
+
+require('./modules/scroll');
+
+require('./modules/methodsES6 for IE11');
+
+require('./modules/jquery.auto-grow-input');
+
+require('./modules/add.hobby');
+
+require('./modules/save.inputs.value');
+
+require('./modules/tabs');
+
+require('./modules/email.validation');
+
+require('./modules/name.validation');
+
+require('./modules/phone.validation');
+
